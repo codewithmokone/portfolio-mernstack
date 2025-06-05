6dd796dd8e404cb0239fa1cd9570353e0bf02c61
Simon Lephoto <codewithmokone@gmail.com>
2025-06-05 10:29:25 +0200

    changed file externsions from js to jsx, inserted code in the footer file.

--- portfolio-frontend/src/App.jsx
+++ portfolio-frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 
 function App() {
 
   return (
     <div className='flex flex-col min-h-screen bg-white'>
       <Header />
-    
+      <main>
+
+      </main>
+      <Footer />
     </div>
   )
 }
---
+++ portfolio-frontend/src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <footer className='bg-gray-900 text-white py-12'>
+            <div className='container mx-auto px-4'>
+                <div className='flex flex-col md:flex-row justify-between items-center'>
+                    <div className='mb-6 md:mb-0'>
+                        <div className='text-2xl font-bold mb-2'>Portfolio</div>
+                    </div>
+                    <div className='flex flex-col items-center md:items-end'>
+                        <div className='flex space-x-4 mb-4'>
+                            <a href=""></a>
+                            <a href=""></a>
+                            <a href=""></a>
+                            <a href=""></a>
+                        </div>
+                        <div>
+                            <p>@ {new Date().getFullYear()} Portfolio. All rights reserved.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
